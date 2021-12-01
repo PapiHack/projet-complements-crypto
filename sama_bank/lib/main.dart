8259092c2c391d6101c6061eef636767bf78943c
PapiHack <mssmbaye@gmail.com>
2021-12-01 09:24:33 +0000

    Remove debug banner from mobile app

--- sama_bank/lib/main.dart
+++ sama_bank/lib/main.dart
@@ -6,18 +6,19 @@ import 'config/config.dart';
 import 'utils/utils.dart';
 
 void main() async => {
-  await EnvironmentConfig.load(),
-  runApp(
-      ProviderScope(
-        child: SamaBankApp(),
+      await EnvironmentConfig.load(),
+      runApp(
+        ProviderScope(
+          child: SamaBankApp(),
+        ),
       ),
-    )
-};
+    };
 
 class SamaBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       scaffoldMessengerKey: rootScaffoldMessengerKey,
       initialRoute: Routes.LOGIN,
       home: LoginPage(),
